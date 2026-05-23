2a0bd543786943a2109d53bb6eebf9e4ade9bb6a
Tasneem younis <tyounes@smail.ucas.edu.ps>
2026-05-23 19:53:07 +0300

    Added scroll to top button

--- script.js
+++ script.js
@@ -41,3 +41,19 @@ function reveal(){
   });
 
 }
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 200) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+};
+
+topBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+};
